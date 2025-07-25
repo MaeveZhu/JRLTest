28c5cc4420305486ebb520e293b4d61e2bf24c10
MaeveZhu <zhuyihan@uw.edu>
2025-07-25 15:25:10 +0800

    Initial Commit

---
+++ JRLTest/JRLTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JRLTestApp.swift
+//  JRLTest
+//
+//  Created by whosyihan on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct JRLTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JRLTestTests/JRLTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  JRLTestTests.swift
+//  JRLTestTests
+//
+//  Created by whosyihan on 7/25/25.
+//
+
+import Testing
+@testable import JRLTest
+
+struct JRLTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
